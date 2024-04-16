8237447ecf4e564739c52ada91b12871249bf31e
KevinFlanagan <flanagank777@gmail.com>
2024-04-16 10:54:38 +0100

    Set env vars for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kw$0p2vj!fa(02js^p6gbw^u=$m^9lx_ib#rcwkvpe48m843)f'
+#SECRET_KEY = 'django-insecure-kw$0p2vj!fa(02js^p6gbw^u=$m^9lx_ib#rcwkvpe48m843)f'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
