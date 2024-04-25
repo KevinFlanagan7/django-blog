e80520ad6484ced68db2c4ad595067a9c37f6899
KevinFlanagan <flanagank777@gmail.com>
2024-04-25 12:21:05 +0100

    Add POST method to database for about form.

--- about/views.py
+++ about/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .models import About
 from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        print("Recieved a POST request")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS,"Collaboration request received! I endeavor to respond within 2 working days.")
+
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
